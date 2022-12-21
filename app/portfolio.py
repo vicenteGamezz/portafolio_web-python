d1fe394075b2a167f703b574ed7a1ee27ef4ce39
vicenteGamezz <ceinnova07721@innovaccion.mx>
2022-12-21 09:27:02 -0700

    Update

--- app/portfolio.py
+++ app/portfolio.py
@@ -1,9 +1,48 @@
 from flask import (
-    Blueprint, render_template
+    Blueprint, redirect, render_template, request, url_for, current_app
 )
+import sendgrid
+from sendgrid.helpers.mail import *
 
 bp = Blueprint('portfolio', __name__, url_prefix='/')
 
-@bp.route('/',methods=['GET'])
+
+@bp.route('/', methods=['GET'])
 def index():
     return render_template('portfolio/index.html')
+
+
+@bp.route('/mail', methods=['GET', 'POST'])
+def mail():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    if request.method == 'POST':
+        send_email(name, email, message)
+        return render_template('portfolio/sent_mail.html')
+
+    return redirect(url_for('portfolio.index'))
+
+
+def send_email(name, email, message):
+    my_email = 'vicente.gamez1@icloud.com'
+    sg = sendgrid.SendGridAPIClient(api_key=current_app.config['SENDGRID_KEY'])
+
+    from_email = Email(my_email)
+    to_email = To('vicente.gamez1@icloud.com', substitutions={
+        "-name-": name,
+        "-email-": email,
+        "-message-": message,
+    })
+
+    html_content = """
+        <p>Hola, Vicente. Tienes un nuevo contacto desde la web:</p>
+        <p>Nombre: -name-</p>
+        <p>Correo: -email-</p>
+        <p>Mensaje: -message-</p>
+    """
+
+    mail = Mail(my_email, to_email, 'Nuevo contacto desde la web', html_content=html_content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    
